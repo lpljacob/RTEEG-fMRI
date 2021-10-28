f102c03e9b4bf6fabfdbe9379e1cc91576044825
lpljacob <lpljacob@scc-x03.scc.bu.edu>
2021-10-28 14:35:47 -0400

    added additional criteria for stim

--- PostprocessingFunctions/SlowWavePhasePredict.m
+++ PostprocessingFunctions/SlowWavePhasePredict.m
@@ -2,11 +2,22 @@ function [vars, Graph, EEG] = SlowWavePhasePredict(EEG, vars, Graph)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+delta_filter = designfilt('bandpassiir', ...
+    'PassbandFrequency1', .8, ... 
+    'Passbandfrequency2', 3, ... 
+    'StopbandFrequency1', .1,...
+    'StopbandFrequency2', 9, ...
+    'StopbandAttenuation1', 30, ...
+    'StopbandAttenuation2', 30, ...
+    'PassbandRipple', 1, ...
+    'DesignMethod', 'butter', ...
+    'SampleRate', EEG.fs);
+[b_delta, a_delta] = tf(delta_filter);
 
 if vars.SamplesInChunk > 0 %&& vars.UseSlowWaveStim
     if ~isfield(vars, 'PhasePredictor')
-        load('LSTM2_shift_75ms.mat')
-        vars.PhasePredictor = resetState(PhaseNet);
+        load('10-26-2021 17-14results_Fpz_s02.mat', 'results');
+        vars.PhasePredictor = resetState(results(1).net);
         vars.SlowWaveDelay = .000;
         vars.Angles = zeros(1000000, 1);
         vars.X = zeros(3, 1000000);
@@ -27,7 +38,7 @@ if vars.SamplesInChunk > 0 %&& vars.UseSlowWaveStim
             sample =  EEG.Kalman_Signal((vars.currentPosition - vars.SamplesInChunk)-1:vars.currentPosition - 1, EEG.KalmanPrimary);
         end
     end
-    [FiltSample, vars.z] = filter(vars.b, vars.a, sample(2:end), vars.z);
+    [FiltSample, vars.z] = filter(vars.b, vars.a, sample(2:end), vars.z); 
     X = zeros(3, length(sample) - 1, 1, 1);
     X(1, :, 1, 1) = sample(2:end);
     X(2, :, 1, 1) = diff(sample);
@@ -41,11 +52,16 @@ if vars.SamplesInChunk > 0 %&& vars.UseSlowWaveStim
     if (vars.currentPosition - vars.TriggerBuffer) > vars.LastStimPosition
         if Mag > EEG.Threshold
             if (PredAngle>=deg2rad(-60) && PredAngle<=deg2rad(-10))
-                PsychPortAudio('Start', vars.audio_port, vars.repetitions, vars.ChunkTime + vars.SlowWaveDelay, 0);
-                %sound(Sound, fsSound)
-                vars.StimTimes(vars.StimCount) = round(vars.currentPosition + vars.SlowWaveDelay * EEG.fs);
-                vars.StimCount = vars.StimCount + 1;
-                vars.LastStimPosition = vars.currentPosition;
+                idx = (vars.currentPosition-EEG.fs*30-1):(vars.currentPosition-1);
+                if mean(envelope(EEG.Recording(idx), length(idx), 'rms')) < 80 % check for mov artifacts
+                    if mean(envelope(filter(b_delta, a_delta, EEG.Recording(idx)), length(idx), 'rms')) > 6 % check for delta
+                        PsychPortAudio('Start', vars.audio_port, vars.repetitions, vars.ChunkTime + vars.SlowWaveDelay, 0);
+                        %sound(Sound, fsSound)
+                        vars.StimTimes(vars.StimCount) = round(vars.currentPosition + vars.SlowWaveDelay * EEG.fs);
+                        vars.StimCount = vars.StimCount + 1;
+                        vars.LastStimPosition = vars.currentPosition;
+                    end
+                end
             end
         end
     end
